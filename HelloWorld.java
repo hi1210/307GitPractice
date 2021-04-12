afe30516d7cbff516be43f6567d750d377de5e79
hi1210 <kungfusnake@gmail.com>
2021-04-12 12:44:59 -0700

    Add years
    - Added the number of years I've been at cal poly

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Heyo World");
+        System.out.println("Heyo World, I've been at CP for 2 years");
     }
 }
 
